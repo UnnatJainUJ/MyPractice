fab84f4f472ad3c1e7bac6890f9db52a8fdbed1e
UnnatJainUJ <unnatjain009@gmail.com>
2024-08-12 14:53:23 +0530

    url module walkthrough

--- NodeJs/server/index.js
+++ NodeJs/server/index.js
@@ -1,11 +1,28 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
   // console.log("New req rec", req.headers);
-  const log = `${Date.now()} : New Req Rec\n`;
+  const log = `${Date.now()} :${req.url} New Req Rec\n`;
+
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
-    res.end("Hello from server huehue");
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("Hello from server huehue");
+        break;
+
+      case "/about":
+        const userName = myUrl.query.myName;
+        res.end(`Hi ${userName}`);
+        break;
+
+      default:
+        res.end("404 Not FOund");
+    }
   });
 });
 
