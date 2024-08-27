f5937375ddf65e2fca6f2adc1f3e5672e087d03b
UnnatJainUJ <unnatjain009@gmail.com>
2024-08-27 19:10:09 +0530

    express basics done

---
+++ NodeJs/server/express_index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Hello from homepage");
+});
+app.get("/about", (req, res) => {
+  return res.send("Hello from about");
+});
+
+app.listen(8000, () => {
+  console.log("Server started.");
+});
+
+
